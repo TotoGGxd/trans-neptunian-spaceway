4b4218aabcf1b5c6a30d7d4311b9b8f14f23e486
Carlos Encinas <carlos-encinas@hotmail.com>
2019-10-19 07:52:36 -0700

    jiji

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
+function myFunction() {
 
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementsByClassName("vertical-menu");
+    li = ul.getElementsByTagName('li');
+   
+  
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+       
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
+
+
+
+document.addEventListener("click", (evt) => {
+    const flyoutElement = document.getElementById("myInput");
+    let targetElement = evt.target; 
+    do {
+        if (targetElement == flyoutElement) {
+            document.getElementsByClassName("vertical-menu").style.display = "block";
+            return;
+        }
+        targetElement = targetElement.parentNode;
+    } while (targetElement);
+
+    document.getElementsByClassName("vertical-menu").style.display = "none";
+});
   
